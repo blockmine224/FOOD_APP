c9446fe86799ea0a71621437ba7a758192210dc4
sleepy1009 <blockmine223@gmail.com>
2025-05-02 11:26:20 +0700

    bruhbruh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { Box } from '@mui/material'; 
-import Introduction from './components/Introduction';
+import Home from './components/Home';
 import Login from './components/Login';
-import TestHistory from './components/TestHistory';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Register from './components/Register';
-import SubjectList from './components/SubjectList';
-import SubjectDetail from './components/SubjectDetail'; 
+ 
 import Account from './components/Account';
-import SubjectQuestions from './components/SubjectQuestions';
-import TestInstructions from './components/TestInstructions';
-import TestTaking from './components/TestTaking';
-import Results from './components/Results';
-import TestDetail from './components/TestDetail';
+
 import { useAuth } from './contexts/AuthContext';
-import './styles/math.css';
+
 import './styles/mui-overrides.css';
 import './styles/main.css';
-import './styles/test.css';
+
 import './App.css';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
@@ -47,18 +42,10 @@ function App() {
         
       }}> 
         <Routes>
-          <Route path="/" element={<Introduction />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/test-history" element={<TestHistory />} />
-          <Route path="/subjects" element={<SubjectList />} />
-          <Route path="/subjects/:id" element={<SubjectDetail />} />
-          <Route path="/test-intro/:subjectId" element={<TestInstructions />} />
           <Route path="/account" element={<Account />} />
-          <Route path="/questions/:subjectId" element={<SubjectQuestions />} />
-          <Route path="/test/:subjectId" element={<TestTaking />} />
-          <Route path="/results/:subjectId" element={<Results />} />
-          <Route path="/test-history/:testId" element={<TestDetail />} />
           <Route path="*" element={isAuthenticated ? <Navigate to="/" /> : <Navigate to="/login" />} />
         </Routes>
       </Box>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 import './styles/main.css';
-import './styles/test.css';
+
 import './styles/mui-overrides.css';
 import './index.css';
 import App from './App';
